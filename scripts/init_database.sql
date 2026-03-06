e3070d469bdff0c84babf307a31d24cffa0068ef
Ruchita Darandale <ruchitadarandale.work@gmail.com>
2026-03-06 19:45:10 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,18 @@
+/*
+Create database and schemas
+
+This scripts helps us create a database named 'DataWarehouse'.
+Additionally, this script also creates 3 schemas under DataWarehouse named as 'bronze', 'silver', 'gold'.
+
+***Warning
+
+Please check if the database already exist in your sql server. 
+*/
+CREATE DATABASE DATAWAREHOUSE;
+
+CREATE SCHEMA bronze;
+
+CREATE SCHEMA silver;
+
+CREATE SCHEMA gold;
 
