72381e6013cc321ca214dfe6c0a8ae99b4d38394
Ruchita Darandale <ruchitadarandale.work@gmail.com>
2026-03-07 20:22:20 +0530

    Create bronze_procedure.sql

---
+++ scripts/bronze_procedure.sql
@@ -0,0 +1,86 @@
+/*
+
+After creating the tables, a stored procedure is created so that we don't need to run the same queries repeatedly.
+
+A stored procedure in SQL is a pre-compiled collection of one or more SQL statements and optional procedural logic 
+(like control flow, variables, and error handling) that is stored in the database management system (DBMS)
+
+Benefits: The same procedure can be called by multiple applications or users, eliminating the need to write the same SQL code repeatedly.
+
+Once stored procedure is created, I have applied the logic to calculate query execution time for each table.
+
+*/
+
+CREATE OR REPLACE PROCEDURE bronze.load_bronze()
+LANGUAGE plpgsql
+AS $$
+DECLARE start_time TIMESTAMP;
+end_time TIMESTAMP;
+
+BEGIN
+
+start_time := CLOCK_TIMESTAMP();
+
+COPY bronze.crm_prd_info 
+FROM 'D:/Work/Sample Datasets/sql-data-warehouse-project/datasets/source_crm/prd_info.csv'
+DELIMITER ','
+CSV HEADER;
+
+end_time := CLOCK_TIMESTAMP();
+RAISE NOTICE 'Execution time for crm_prd_info: % seconds', EXTRACT(EPOCH FROM end_time - start_time);
+
+start_time := CLOCK_TIMESTAMP();
+
+COPY bronze.crm_cust_info 
+FROM 'D:/Work/Sample Datasets/sql-data-warehouse-project/datasets/source_crm/cust_info.csv'
+DELIMITER ','
+CSV HEADER;
+
+end_time := CLOCK_TIMESTAMP();
+RAISE NOTICE 'Execution time for crm_cust_info: % seconds', EXTRACT(EPOCH FROM end_time - start_time);
+
+start_time := CLOCK_TIMESTAMP();
+
+COPY bronze.crm_sales_details
+FROM 'D:/Work/Sample Datasets/sql-data-warehouse-project/datasets/source_crm/sales_details.csv'
+DELIMITER ','
+CSV HEADER;
+
+end_time := CLOCK_TIMESTAMP();
+RAISE NOTICE 'Execution time for crm_sales_details: % seconds', EXTRACT(EPOCH FROM end_time - start_time);
+
+start_time := CLOCK_TIMESTAMP();
+
+COPY bronze.erp_cust_az12
+FROM 'D:/Work/Sample Datasets/sql-data-warehouse-project/datasets/source_erp/cust_az12.csv'
+DELIMITER ','
+CSV HEADER;
+
+end_time := CLOCK_TIMESTAMP();
+RAISE NOTICE 'Execution time for cust_az12: % seconds', EXTRACT(EPOCH FROM end_time - start_time);
+
+start_time := CLOCK_TIMESTAMP();
+
+COPY bronze.erp_px_cat_g1v2
+FROM 'D:/Work/Sample Datasets/sql-data-warehouse-project/datasets/source_erp/px_cat_g1v2.csv'
+DELIMITER ','
+CSV HEADER;
+
+end_time := CLOCK_TIMESTAMP();
+RAISE NOTICE 'Execution time for erp_px_cat_g1v2: % seconds', EXTRACT(EPOCH FROM end_time - start_time);
+
+start_time := CLOCK_TIMESTAMP();
+
+COPY bronze.erp_loc_a101
+FROM 'D:/Work/Sample Datasets/sql-data-warehouse-project/datasets/source_erp/loc_a101.csv'
+DELIMITER ','
+CSV HEADER;
+
+end_time := CLOCK_TIMESTAMP();
+RAISE NOTICE 'Execution time for erp_loc_a101: % seconds', EXTRACT(EPOCH FROM end_time - start_time);
+
+
+END;
+$$;
+
+CALL bronze.load_bronze();
