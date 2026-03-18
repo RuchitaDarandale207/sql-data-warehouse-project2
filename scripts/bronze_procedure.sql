65e12c847ada0d5a830aefcccc0c369571ae1d3e
Ruchita Darandale <ruchitadarandale.work@gmail.com>
2026-03-18 13:38:53 +0530

    Update bronze_procedure.sql

--- scripts/bronze_procedure.sql
+++ scripts/bronze_procedure.sql
@@ -16,11 +16,14 @@ LANGUAGE plpgsql
 AS $$
 DECLARE start_time TIMESTAMP;
 end_time TIMESTAMP;
+batch_start_time TIMESTAMP;
+batch_end_time TIMESTAMP;
 
 BEGIN
-
+batch_start_time := CLOCK_TIMESTAMP();
 start_time := CLOCK_TIMESTAMP();
 
+TRUNCATE TABLE bronze.crm_prd_info;
 COPY bronze.crm_prd_info 
 FROM 'D:/Work/Sample Datasets/sql-data-warehouse-project/datasets/source_crm/prd_info.csv'
 DELIMITER ','
@@ -31,6 +34,7 @@ RAISE NOTICE 'Execution time for crm_prd_info: % seconds', EXTRACT(EPOCH FROM en
 
 start_time := CLOCK_TIMESTAMP();
 
+TRUNCATE TABLE bronze.crm_cust_info;
 COPY bronze.crm_cust_info 
 FROM 'D:/Work/Sample Datasets/sql-data-warehouse-project/datasets/source_crm/cust_info.csv'
 DELIMITER ','
@@ -41,6 +45,7 @@ RAISE NOTICE 'Execution time for crm_cust_info: % seconds', EXTRACT(EPOCH FROM e
 
 start_time := CLOCK_TIMESTAMP();
 
+TRUNCATE TABLE bronze.srm_sales_details;
 COPY bronze.crm_sales_details
 FROM 'D:/Work/Sample Datasets/sql-data-warehouse-project/datasets/source_crm/sales_details.csv'
 DELIMITER ','
@@ -51,6 +56,7 @@ RAISE NOTICE 'Execution time for crm_sales_details: % seconds', EXTRACT(EPOCH FR
 
 start_time := CLOCK_TIMESTAMP();
 
+TRUNCATE TABLE bronze.erp_cust_az12;
 COPY bronze.erp_cust_az12
 FROM 'D:/Work/Sample Datasets/sql-data-warehouse-project/datasets/source_erp/cust_az12.csv'
 DELIMITER ','
@@ -61,6 +67,7 @@ RAISE NOTICE 'Execution time for cust_az12: % seconds', EXTRACT(EPOCH FROM end_t
 
 start_time := CLOCK_TIMESTAMP();
 
+TRUNCATE TABLE bronze.erp_px_cat_g1v2;
 COPY bronze.erp_px_cat_g1v2
 FROM 'D:/Work/Sample Datasets/sql-data-warehouse-project/datasets/source_erp/px_cat_g1v2.csv'
 DELIMITER ','
@@ -71,6 +78,7 @@ RAISE NOTICE 'Execution time for erp_px_cat_g1v2: % seconds', EXTRACT(EPOCH FROM
 
 start_time := CLOCK_TIMESTAMP();
 
+TRUNCATE TABLE bronze.erp_loc_a101;
 COPY bronze.erp_loc_a101
 FROM 'D:/Work/Sample Datasets/sql-data-warehouse-project/datasets/source_erp/loc_a101.csv'
 DELIMITER ','
@@ -78,9 +86,11 @@ CSV HEADER;
 
 end_time := CLOCK_TIMESTAMP();
 RAISE NOTICE 'Execution time for erp_loc_a101: % seconds', EXTRACT(EPOCH FROM end_time - start_time);
-
+batch_end_time := CLOCK_TIMESTAMP();
+RAISE NOTICE 'Execution time for whole batch: % seconds', EXTRACT(EPOCH FROM batch_end_time - batch_start_time);
 
 END;
 $$;
 
 CALL bronze.load_bronze();
+
